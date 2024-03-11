66b95122ef87ab1331d237f79f2c453f515bb6ae
Joe Ellul-Turner <joe@joes-air.home>
2024-03-11 17:33:24 +0000

    nav style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,16 +12,20 @@
 
 <main>
     <nav
-        class="sticky top-0 left-0 z-20 flex w-full items-center justify-between bg-black p-3"
+        class="sticky top-0 left-0 z-20 flex w-full items-center justify-between bg-black py-3 px-4"
     >
-        <a href="/"
-            ><img
-                src={Icon}
-                width="70"
-                class="rounded hover:ring"
-                alt=""
-            />
+        <a href="/">
+            <div class="flex items-center gap-2">
+                <img
+                    src={Icon}
+                    width="50"
+                    class="rounded"
+                    alt=""
+                />
+                <h1 class="text-4xl font-bold text-yellow-500">Monstoorz</h1>
+            </div>
         </a>
+
         <div class="flex items-center gap-6">
             {#each NAVLINKS as { path, label }}
                 <a
